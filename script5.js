2b8f9f77fafd3387422e0cb22cf3a8e5821a0e33
eliteAcres <fooklitugboat2@hotmail.com>
2023-10-17 16:40:01 +0800

    Update monster game file

--- script5.js
+++ script5.js
@@ -1,130 +1,209 @@
-const canvas = document.querySelector("canvas"),
-toolBtns = document.querySelectorAll(".tool"),
-fillColor = document.querySelector("#fill-color"),
-sizeSlider = document.querySelector("#size-slider"),
-colorBtns = document.querySelectorAll(".colors .option"),
-colorPicker = document.querySelector("#color-picker"),
-clearCanvas = document.querySelector(".clear-canvas"),
-saveImg = document.querySelector(".save-img"),
-ctx = canvas.getContext("2d");
+// Initialize canvas
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
 
-// global variables with default value
-let prevMouseX, prevMouseY, snapshot,
-isDrawing = false,
-selectedTool = "brush",
-brushWidth = 5,
-selectedColor = "#000";
+var startBtn = document.getElementById("start-btn");
+var pauseBtn = document.getElementById("pause-btn");
+var restartBtn = document.getElementById("restart-btn");
+var animationId;
+var gameRunning = false;
 
-const setCanvasBackground = () => {
-    // setting whole canvas background to white, so the downloaded img background will be white
-    ctx.fillStyle = "#fff";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = selectedColor; // setting fillstyle back to the selectedColor, it'll be the brush color
-}
-
-window.addEventListener("load", () => {
-    // setting canvas width/height.. offsetwidth/height returns viewable width/height of an element
-    canvas.width = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
-    setCanvasBackground();
+startBtn.addEventListener("click", function() {
+  if (!gameRunning) { // only start the game if gameRunning is false
+    gameRunning = true; // set gameRunning to true when the game starts
+    loop();
+  }
 });
 
-const drawRect = (e) => {
-    // if fillColor isn't checked draw a rect with border else draw rect with background
-    if(!fillColor.checked) {
-        // creating circle according to the mouse pointer
-        return ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
-    }
-    ctx.fillRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
-}
-
-const drawCircle = (e) => {
-    ctx.beginPath(); // creating new path to draw circle
-    // getting radius for circle according to the mouse pointer
-    let radius = Math.sqrt(Math.pow((prevMouseX - e.offsetX), 2) + Math.pow((prevMouseY - e.offsetY), 2));
-    ctx.arc(prevMouseX, prevMouseY, radius, 0, 2 * Math.PI); // creating circle according to the mouse pointer
-    fillColor.checked ? ctx.fill() : ctx.stroke(); // if fillColor is checked fill circle else draw border circle
-}
-
-const drawTriangle = (e) => {
-    ctx.beginPath(); // creating new path to draw circle
-    ctx.moveTo(prevMouseX, prevMouseY); // moving triangle to the mouse pointer
-    ctx.lineTo(e.offsetX, e.offsetY); // creating first line according to the mouse pointer
-    ctx.lineTo(prevMouseX * 2 - e.offsetX, e.offsetY); // creating bottom line of triangle
-    ctx.closePath(); // closing path of a triangle so the third line draw automatically
-    fillColor.checked ? ctx.fill() : ctx.stroke(); // if fillColor is checked fill triangle else draw border
-}
-
-const startDraw = (e) => {
-    isDrawing = true;
-    prevMouseX = e.offsetX; // passing current mouseX position as prevMouseX value
-    prevMouseY = e.offsetY; // passing current mouseY position as prevMouseY value
-    ctx.beginPath(); // creating new path to draw
-    ctx.lineWidth = brushWidth; // passing brushSize as line width
-    ctx.strokeStyle = selectedColor; // passing selectedColor as stroke style
-    ctx.fillStyle = selectedColor; // passing selectedColor as fill style
-    // copying canvas data & passing as snapshot value.. this avoids dragging the image
-    snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
-}
-
-const drawing = (e) => {
-    if(!isDrawing) return; // if isDrawing is false return from here
-    ctx.putImageData(snapshot, 0, 0); // adding copied canvas data on to this canvas
-
-    if(selectedTool === "brush" || selectedTool === "eraser") {
-        // if selected tool is eraser then set strokeStyle to white 
-        // to paint white color on to the existing canvas content else set the stroke color to selected color
-        ctx.strokeStyle = selectedTool === "eraser" ? "#fff" : selectedColor;
-        ctx.lineTo(e.offsetX, e.offsetY); // creating line according to the mouse pointer
-        ctx.stroke(); // drawing/filling line with color
-    } else if(selectedTool === "rectangle"){
-        drawRect(e);
-    } else if(selectedTool === "circle"){
-        drawCircle(e);
-    } else {
-        drawTriangle(e);
-    }
-}
-
-toolBtns.forEach(btn => {
-    btn.addEventListener("click", () => { // adding click event to all tool option
-        // removing active class from the previous option and adding on current clicked option
-        document.querySelector(".options .active").classList.remove("active");
-        btn.classList.add("active");
-        selectedTool = btn.id;
-    });
+pauseBtn.addEventListener("click", function() {
+  gameRunning = false;
+  cancelAnimationFrame(animationId);
 });
 
-sizeSlider.addEventListener("change", () => brushWidth = sizeSlider.value); // passing slider value as brushSize
-
-colorBtns.forEach(btn => {
-    btn.addEventListener("click", () => { // adding click event to all color button
-        // removing selected class from the previous option and adding on current clicked option
-        document.querySelector(".options .selected").classList.remove("selected");
-        btn.classList.add("selected");
-        // passing selected btn background color as selectedColor value
-        selectedColor = window.getComputedStyle(btn).getPropertyValue("background-color");
-    });
+restartBtn.addEventListener("click", function() {
+  document.location.reload();
 });
 
-colorPicker.addEventListener("change", () => {
-    // passing picked color value from color picker to last color btn background
-    colorPicker.parentElement.style.background = colorPicker.value;
-    colorPicker.parentElement.click();
+addEventListener("load", (event) => {
+  draw();
 });
 
-clearCanvas.addEventListener("click", () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height); // clearing whole canvas
-    setCanvasBackground();
-});
 
-saveImg.addEventListener("click", () => {
-    const link = document.createElement("a"); // creating <a> element
-    link.download = `${Date.now()}.jpg`; // passing current date as link download value
-    link.href = canvas.toDataURL(); // passing canvasData as link href value
-    link.click(); // clicking link to download image
-});
+// Define ball properties
+var ballRadius = 10;
+var ballX = canvas.width / 2;
+var ballY = canvas.height / 2;
+var ballSpeedX = 5;
+var ballSpeedY = 5;
 
-canvas.addEventListener("mousedown", startDraw);
-canvas.addEventListener("mousemove", drawing);
-canvas.addEventListener("mouseup", () => isDrawing = false);
+// Define paddle properties
+var paddleHeight = 80;
+var paddleWidth = 10;
+var leftPaddleY = canvas.height / 2 - paddleHeight / 2;
+var rightPaddleY = canvas.height / 2 - paddleHeight / 2;
+var paddleSpeed = 10;
+
+// Define score properties
+var leftPlayerScore = 0;
+var rightPlayerScore = 0;
+var maxScore = 10;
+
+// Listen for keyboard events
+document.addEventListener("keydown", keyDownHandler);
+document.addEventListener("keyup", keyUpHandler);
+
+// Handle key press
+var upPressed = false;
+var downPressed = false;
+let wPressed = false;
+let sPressed = false;
+
+function keyDownHandler(e) {
+  if (e.key === "ArrowUp") {
+    upPressed = true;
+  } else if (e.key === "ArrowDown") {
+    downPressed = true;
+  } else if (e.key === "w") {
+    wPressed = true;
+  } else if (e.key === "s") {
+    sPressed = true;
+  }
+}
+
+// Handle key release
+function keyUpHandler(e) {
+  if (e.key === "ArrowUp") {
+    upPressed = false;
+  } else if (e.key === "ArrowDown") {
+    downPressed = false;
+  } else if (e.key === "w") {
+    wPressed = false;
+  } else if (e.key === "s") {
+    sPressed = false;
+  }
+}
+
+// Update game state
+function update() {
+  // Move paddles
+  if (upPressed && rightPaddleY > 0) {
+    rightPaddleY -= paddleSpeed;
+  } else if (downPressed && rightPaddleY + paddleHeight < canvas.height) {
+    rightPaddleY += paddleSpeed;
+  }
+
+  // Move right paddle based on "w" and "s" keys
+  if (wPressed && leftPaddleY > 0) {
+    leftPaddleY -= paddleSpeed;
+  } else if (sPressed && leftPaddleY + paddleHeight < canvas.height) {
+    leftPaddleY += paddleSpeed;
+  }
+
+  // Move right paddle automatically based on ball position
+  // if (ballY > rightPaddleY + paddleHeight / 2) {
+  //   rightPaddleY += paddleSpeed;
+  // } else if (ballY < rightPaddleY + paddleHeight / 2) {
+  //   rightPaddleY -= paddleSpeed;
+  // }
+
+  // Move ball
+  ballX += ballSpeedX;
+  ballY += ballSpeedY;
+
+  // Check if ball collides with top or bottom of canvas
+  if (ballY - ballRadius < 0 || ballY + ballRadius > canvas.height) {
+    ballSpeedY = -ballSpeedY;
+  }
+
+  // Check if ball collides with left paddle
+  if (
+    ballX - ballRadius < paddleWidth &&
+    ballY > leftPaddleY &&
+    ballY < leftPaddleY + paddleHeight
+  ) {
+    ballSpeedX = -ballSpeedX;
+  }
+
+  // Check if ball collides with right paddle
+  if (
+    ballX + ballRadius > canvas.width - paddleWidth &&
+    ballY > rightPaddleY &&
+    ballY < rightPaddleY + paddleHeight
+  ) {
+    ballSpeedX = -ballSpeedX;
+  }
+
+  // Check if ball goes out of bounds on left or right side of canvas
+  if (ballX < 0) {
+    rightPlayerScore++;
+    reset();
+  } else if (ballX > canvas.width) {
+    leftPlayerScore++;
+    reset();
+  }
+
+  // Check if a player has won
+  if (leftPlayerScore === maxScore) {
+    playerWin("Left player");
+  } else if (rightPlayerScore === maxScore) {
+    playerWin("Right player");
+  }
+}
+
+function playerWin(player) {
+  var message = "Congratulations! " + player + " win!";
+  $('#message').text(message); // Set the message text
+  $('#message-modal').modal('show'); // Display the message modal
+  reset();
+}
+
+// Reset ball to center of screen
+function reset() {
+  ballX = canvas.width / 2;
+  ballY = canvas.height / 2;
+  ballSpeedX = -ballSpeedX;
+  ballSpeedY = Math.random() * 10 - 5;
+}
+
+// Draw objects on canvas
+function draw() {
+  // Clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "#FFF";
+  ctx.font = "15px Arial";
+
+  ctx.beginPath();
+  ctx.moveTo(canvas.width / 2, 0);
+  ctx.lineTo(canvas.width / 2, canvas.height);
+  ctx.strokeStyle = "#FFF"; // Set line color to white
+  ctx.stroke();
+  ctx.closePath();
+
+  // Draw ball
+  ctx.beginPath();
+  ctx.arc(ballX, ballY, ballRadius, 0, Math.PI * 2);
+  ctx.fill();
+  ctx.closePath();
+
+  // Draw left paddle
+  ctx.fillRect(0, leftPaddleY, paddleWidth, paddleHeight);
+
+  // Draw right paddle
+  ctx.fillRect(canvas.width - paddleWidth, rightPaddleY, paddleWidth, paddleHeight);
+
+  // Draw scores
+  ctx.fillText("Score: " + leftPlayerScore, 10, 20);
+  ctx.fillText("Score: " + rightPlayerScore, canvas.width - 70, 20);
+}
+
+// Game loop
+function loop() {
+  update();
+  draw();
+  animationId = requestAnimationFrame(loop);
+}
+
+$('#message-modal-close').on('click', function() {
+  document.location.reload();
+});
---
+++ script7.js
@@ -0,0 +1,165 @@
+/*  
+  Code modified from:
+  http://www.lostdecadegames.com/how-to-make-a-simple-html5-canvas-game/
+  using graphics purchased from vectorstock.com
+*/
+
+// Create the canvas for the game to display in
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = 512;
+canvas.height = 480;
+document.body.appendChild(canvas);
+
+// Load the background image
+var bgReady = false;
+var bgImage = new Image();
+bgImage.onload = function () {
+  // show the background image
+  bgReady = true;
+};
+bgImage.src = "monster/background.png";
+
+// Load the player image
+var playerReady = false;
+var playerImage = new Image();
+playerImage.onload = function () {
+  // show the player image
+  playerReady = true;
+};
+playerImage.src = "monster/player.png";
+
+// Load the enemy image
+var enemyReady = false;
+var enemyImage = new Image();
+enemyImage.onload = function () {
+  // show the enemy image
+  enemyReady = true;
+};
+enemyImage.src = "monster/enemy.png?format=300w";
+
+// Create the game objects
+var player = {
+  speed: 256 // movement speed of player in pixels per second
+};
+var enemy = {};
+var enemiesCaught = 0;
+
+// Handle keyboard controls
+var keysDown = {};
+
+// Check for keys pressed where key represents the key pressed
+addEventListener("keydown", function (event) {
+  keysDown[event.key] = true;
+}, false);
+
+addEventListener("keyup", function (event) {
+  delete keysDown[event.key];
+}, false);
+
+// Reset the player and enemy positions when player catches an enemy
+var reset = function () {
+  // Reset player's position to centre of canvas
+  player.x = canvas.width / 2;
+  player.y = canvas.height / 2;
+
+  // Place the enemy somewhere on the canvas randomly
+  enemy.x = enemyImage.width + (Math.random() * (canvas.width - (enemyImage.width*2)));
+  enemy.y = enemyImage.height + (Math.random() * (canvas.height - (enemyImage.height*2)));
+};
+
+// Update game objects - change player position based on key pressed
+var update = function (modifier) {
+  if ("ArrowUp" in keysDown || "w" in keysDown) { // Player is holding up key
+    player.y -= player.speed * modifier;
+  }
+  if ("ArrowDown" in keysDown || "s" in keysDown) { // Player is holding down key
+    player.y += player.speed * modifier;
+  }
+  if ("ArrowLeft" in keysDown || "a" in keysDown) { // Player is holding left key
+    player.x -= player.speed * modifier;
+  }
+  if ("ArrowRight" in keysDown || "d" in keysDown) { // Player is holding right key
+    player.x += player.speed * modifier;
+  }
+
+  // Check if player and enemy collide
+  if (
+    player.x <= (enemy.x + enemyImage.width)
+    && enemy.x <= (player.x + playerImage.width)
+    && player.y <= (enemy.y + enemyImage.height)
+    && enemy.y <= (player.y + playerImage.height)
+  ) {
+    ++enemiesCaught;
+    reset();
+  }
+};
+
+// Draw everything on the canvas
+var render = function () {
+  if (bgReady) {
+    ctx.drawImage(bgImage, 0, 0);
+  }
+
+  if (playerReady) {
+    ctx.drawImage(playerImage, player.x, player.y);
+  }
+
+  if (enemyReady) {
+    ctx.drawImage(enemyImage, enemy.x, enemy.y);
+  }
+
+  // Display score and time 
+  ctx.fillStyle = "rgb(250, 250, 250)";
+  ctx.font = "24px Helvetica";
+  ctx.textAlign = "left";
+  ctx.textBaseline = "top";
+  ctx.fillText("Enemies caught: " + enemiesCaught, 20, 20);
+  ctx.fillText("Time: " + count, 20, 50);
+  // Display game over message when timer finished
+  if(finished==true){
+    ctx.fillText("Game over!", 200, 220);
+  }
+
+  
+};
+
+var count = 30; // how many seconds the game lasts for - default 30
+var finished = false;
+var counter =function(){
+  count=count-1; // countown by 1 every second
+  // when count reaches 0 clear the timer, hide enemy and player and finish the game
+  	if (count <= 0)
+  	{
+  		// stop the timer
+     	clearInterval(counter);
+     	// set game to finished
+     	finished = true;
+     	count=0;
+     	// hider enemy and player
+     	enemyReady=false;
+     	playerReady=false;
+  	}
+
+}
+
+// timer interval is every second (1000ms)
+setInterval(counter, 1000);
+
+// The main game loop
+var main = function () {
+  // run the update function
+  update(0.02); // do not change
+  // run the render function
+  render();
+  // Request to do this again ASAP
+  requestAnimationFrame(main);
+};
+
+// Cross-browser support for requestAnimationFrame
+var w = window;
+requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+
+// Let's play this game!
+reset();
+main();
